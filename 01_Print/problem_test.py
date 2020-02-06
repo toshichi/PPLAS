d8d936d3be6ab7735c45ec21b76025f109b2e3ac
Sykie Chen <chen17@coder17.com>
2020-02-07 01:05:45 +0900

    add: printing test

---
+++ 01_Print/problem_test.py
@@ -0,0 +1,20 @@
+import sys
+import unittest
+from io import StringIO
+
+
+class ProblemTest(unittest.TestCase):
+    def setUp(self):
+        self.captor = StringIO()
+        sys.stdout = self.captor
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+
+    def test_main(self):
+        import problem
+        self.assertEqual(self.captor.getvalue(), 'Hello World!\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
